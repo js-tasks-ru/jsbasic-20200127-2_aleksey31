ecc4d649f3c0d32346d1f2e8569e17366616e453
Alexey <salomatin88@bk.ru>
2020-02-02 22:30:50 +0300

    Исправлена ошибка при валидации

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (name.length > 3 && name != null && !name.includes(" ")) {
+  if (name && name.length > 3 && name != null && !name.includes(" ")) {
     return true;
   }
   return false;
