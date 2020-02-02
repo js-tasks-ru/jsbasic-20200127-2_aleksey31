8631bafedf5e251ed8ce4a701bd7c9e224278802
Alexey <salomatin88@bk.ru>
2020-02-02 21:56:55 +0300

    Сделана задача факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,17 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
+'use strict';
 
+function factorial(n) {
+    let result = n;
+    let p = n;
+    if (n < 1) {
+        result = 1;
+    } else {
+        for (let i = 1; i < n; i++) {
+            result = result * (--p);
+        }
+    }
+    return result;
 }
