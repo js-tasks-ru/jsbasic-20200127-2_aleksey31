a3eab76fad682abfe911f3a8d333d3cdf052b2c0
Alexey <salomatin88@bk.ru>
2020-02-02 22:53:50 +0300

    Проверка на спам.

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,12 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
+    let xBet = '1xBet',
+        xxx = 'XXX';
 
+    str = str.toLowerCase();
+    if(str.includes(xBet.toLowerCase()) || str.includes(xxx.toLowerCase())) {
+        return true;
+    }
+    return false;
 }
