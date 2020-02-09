1e732ad34bc12a81d8fcc2ffad44c2e6ba8b3dc5
Alexey <salomatin88@bk.ru>
2020-02-09 12:47:32 +0300

    Преобразование в верблюжью нотацию.

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,10 @@
  * @returns {string}
  */
 function camelize(str) {
+    let newArr = str.toLowerCase().split("-");
+
+    let result = newArr.filter((item) => item)
+        .map((item, index, arr) => index > 0 ? arr[index] = item[0].toUpperCase() + item.slice(1) : item);
+
+    return result.join("");
 }
