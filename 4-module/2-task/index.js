d4a52fdf1eb76a29fb9b531ff7d947f84401bb0b
Alexey <salomatin88@bk.ru>
2020-02-12 23:35:25 +0300

    Красные ячейки

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,13 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    let rows = Array.from(table.rows);
+    rows.forEach (function(row, rowIndex) {
+        let cell = Array.from(row.cells);
+        cell.forEach(function(cell, cellIndex, arr) {
+            if (cellIndex == rowIndex) {
+                arr[cellIndex].style.backgroundColor = "red";
+            }
+        });
+    });
 }
