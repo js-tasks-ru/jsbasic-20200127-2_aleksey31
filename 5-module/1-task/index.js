b8dd59f05022676e552a8c63fb72710be5cae5cd
Alexey <salomatin88@bk.ru>
2020-02-19 23:24:01 +0300

    color cell

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,29 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let tbody = table.querySelector("tbody");
+    let tr = tbody.rows;
+    for (let item of tr) {
+        if (item.cells[3].hasAttribute("data-available")) {
+            if (item.cells[3].dataset.available === "true") {
+                item.classList.add('available');
+            } else if (item.cells[3].dataset.available === "false") {
+                item.classList.add('unavailable');
+            }
+
+            if (item.cells[2].innerHTML === "f") {
+                item.classList.add('female');
+            } else if (item.cells[2].innerHTML === "m") {
+                item.classList.add('male');
+            }
+
+            if (item.cells[1].innerHTML < 18 ) {
+                item.style.textDecoration = "line-through";
+            }
+
+        } else {
+            item.setAttribute("hidden", "");
+        }
+
+    }
 }
