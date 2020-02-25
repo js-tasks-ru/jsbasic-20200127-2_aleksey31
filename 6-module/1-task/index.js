bab959e809294226f2c451d62eb13d225433f7bd
Alexey <salomatin88@bk.ru>
2020-02-25 23:21:38 +0300

    Компонент очищаемая таблица

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -17,13 +17,42 @@ class ClearedTable {
   constructor(data) {
     this.el = document.createElement('table');
     this.data = data;
+
+    this.el.classList.add("pure-table");
+    this.el.innerHTML = `<thead>
+                                <tr>
+                                    <td>Name</td><td>Age</td><td>Salary</td><td>City</td><td></td>
+                                </tr>
+                            </thead>
+                         <tbody></tbody>`;
+    let tBody = this.el.querySelector("tbody");
+
+    data.forEach((item, index) => {
+      let row = document.createElement('tr');
+      row.innerHTML = `<td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td><td><a href="" data-close="${item.id}">X</a></td>`;
+      tBody.append(row);
+    });
+
+    this.el.addEventListener("click", rowRemove);
+    this.el.addEventListener("click", (e) => this.onRemoved(e.target.dataset.close));
+    function rowRemove(e) {
+      e.preventDefault();
+      if (e.target.tagName === "A") {
+        e.target.closest("tr").remove();
+        let returnId = new Event("my-event");
+        e.target.dispatchEvent(returnId);
+      }
+    }
   }
+
 
   /**
    * Метод который вызывается после удалении строки
    * @param {number} id - идентификатор удаляемого пользователя
    */
-  onRemoved(id) {}
+  onRemoved(id) {
+     console.log(`Из таблицы удален пользователь ${id}`);
+  }
 }
 
 window.ClearedTable = ClearedTable;
