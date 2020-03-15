a814d3bb11f95c8ff5a8203cf4565390693d1af3
Alexey <salomatin88@bk.ru>
2020-03-15 20:30:56 +0300

    Промисификация кнопки 7

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -4,11 +4,12 @@
  * @returns {Promise}
  */
 
+"use strict"
+const but = document.querySelector('button');
 function promiseClick(button) {
-    button.addEventListener('click', (event) => {
-        let promise = new Promise(function(resolve) {
-            resolve(event);
-        });
-        promise.then((event) => console.log(event));
-    }, { once: true });
+    let promise = new Promise(function(resolve) {
+        button.addEventListener('click', (event) => resolve(event), { once: true });
+    });
+    return promise;
 }
+promiseClick(but).then((event) => console.log(event));
